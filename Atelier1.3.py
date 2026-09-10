602c03a697a92ca75cabab06b24058b52944f7c6
Theo-049 <theo.metayer044@gmail.com>
2026-09-10 11:17:02 +0200

    modif:ligne

---
+++ Atelier1.3.py
@@ -0,0 +1,18 @@
+#Ajoute du texte dans la nouvelle branche
+unites = {
+    "mg": 0.001,
+    "cg": 0.01,
+    "dg": 0.1,
+    "g": 1,
+    "dag": 10,
+    "hg": 100,
+    "kg": 1000,
+    "t": 10**4
+}
+
+#je suis la même personne
+
+def conversion_masse(unité_actuelle, nouvelle_unité, poids):
+    return poids * unites[unité_actuelle] / unites[nouvelle_unité]
+
+print(conversion_masse("hg", "kg", 1))
